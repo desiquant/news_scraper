77bb2535f309017af727f2959e563ba99636555b
skd <skdcodes@gmail.com>
2024-09-16 14:13:47 +0530

    add: script to scrape all

---
+++ scrape.py
@@ -0,0 +1,42 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from news_scraper.spiders import (
+    BusinessStandardSpider,
+    BusinessTodaySpider,
+    EconomicTimesSpider,
+    FinancialExpressSpider,
+    FirstPostSpider,
+    FreePressJournalSpider,
+    IndianExpressSpider,
+    MoneyControlSpider,
+    NDTVProfitSpider,
+    News18Spider,
+    OutlookIndiaSpider,
+    TheHinduBusinessLineSpider,
+    TheHinduSpider,
+    ZeeNewsSpider,
+)
+
+settings = get_project_settings()
+
+settings.update({"LOG_FILE": "scrape.log"})
+
+process = CrawlerProcess(settings=settings)
+
+process.crawl(BusinessStandardSpider)
+process.crawl(BusinessTodaySpider)
+process.crawl(EconomicTimesSpider)
+process.crawl(FinancialExpressSpider)
+process.crawl(FirstPostSpider)
+process.crawl(FreePressJournalSpider)
+# process.crawl(IndianExpressSpider)
+# process.crawl(MoneyControlSpider)
+process.crawl(NDTVProfitSpider)
+process.crawl(News18Spider)
+process.crawl(OutlookIndiaSpider)
+process.crawl(TheHinduSpider)
+process.crawl(TheHinduBusinessLineSpider)
+process.crawl(ZeeNewsSpider)
+
+process.start()
